37fd71a9a4dfa1a0b1516d237b5d1f18f6dfff32
Timothy Warnock <tim@timwarnock.com>
2018-01-25 22:40:22 -0800

    separated files from index.html

--- zhcz/json.php
+++ zhcz/json.php
@@ -8,7 +8,12 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');  
 
 $CSV = $_REQUEST['input'];
+// check if csv exists here or in data directory
 if (!file_exists($CSV)) {
+  $CSV = "data/$CSV";
+}
+if (!file_exists($CSV)) {
+  // can't find csv, give up
   http_response_code(404);
   die();
 }
