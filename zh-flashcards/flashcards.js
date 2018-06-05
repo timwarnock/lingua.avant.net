104f9607fef33b13fe5a1b7ddc3ea3c6f333a057
Timothy Warnock <tim@timwarnock.com>
2018-06-05 06:05:10 -0700

    swipe right event, experimental

--- zh-flashcards/flashcards.js
+++ zh-flashcards/flashcards.js
@@ -479,3 +479,46 @@ document.onkeydown = function(evt) {
 };
 
 
+// swipe right - EXPERIMENTAL
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function handleTouchStart(evt) {
+    console.log('touch start');
+    xDown = evt.touches[0].clientX;
+    yDown = evt.touches[0].clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */ 
+            flipNext();
+        } else {
+            /* left swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+        } else { 
+            /* down swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
